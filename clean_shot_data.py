93c6468dec39ede3db2b63a22621f93844da0a38
Samir Wadhwania <swadhwania96@gmail.com>
2021-04-17 13:41:57 -0400

    Only need to be symmetric across half court, whoops

--- clean_shot_data.py
+++ clean_shot_data.py
@@ -8,8 +8,11 @@ with open("src/shot_pct.json") as json_file:
 new_dict = {}
 for k1, v1 in data.items():
     new_dict[k1] = {}
-    new_dict[str(49-int(k1))] = {}
     for k2, v2 in v1.items():
+
+        if int(k2) > 46:
+            continue
+
         arr = []
         for team, val in v2.items():
             if val != None:
@@ -17,7 +20,7 @@ for k1, v1 in data.items():
             else: 
                 arr.append({"name": team, "val": 0})
         new_dict[k1][k2] = arr
-        new_dict[str(49-int(k1))][str(93-int(k2))] = arr
+        new_dict[k1][str(93-int(k2))] = arr
 
 with open("src/shot_pct_cleaned.json", "w") as outfile:
     json.dump(new_dict, outfile)
